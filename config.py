431c6a17f805ddd8fc9094af8f2b88ac3eaea490
Ryan <81808867+ryu878@users.noreply.github.com>
2023-02-11 17:10:30 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Bybit settings
+domain = 'bybit'
+endpoint = 'https://api.bybit.com'
+api_key = ''
+api_secret = ''
+interval = 1
+limit = 200
+category = 'linear'
+
+# Database settings
+redis_host = 'localhost'
+redis_port = 6379
+redis_db = 0
+redis_trade_ttl = 333
+redis_kline_ttl = 14400
